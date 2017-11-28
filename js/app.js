63bc6e79c29a1e5b5452dff412e3a52aaad58c5e
PaolaFU <paola_sfu@hotmail.com>
2017-11-28 17:22:59 -0500

    Modificando archivos

--- js/app.js
+++ js/app.js
@@ -3,4 +3,13 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+console.log(data.AQP);
+console.log(data.LIM);
+console.log(data.CDMX);
+console.log(data.SCL);
+/* var AQP = data.AQP["2017-2"];*/
+var arequipa = data.AQP;
+var lima = data.LIM;
+var mexico = data.CDMX;
+var chile = data.SCL;
+/* console.log(AQP); */
